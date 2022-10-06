a4007afac57e6ef746c2f3a3770d5eeb97c69ec2
jhhocs <joshuaho1@cpp.edu>
2022-10-05 19:22:21 -0700

    Added comments

---
+++ TicTacToe.c
@@ -0,0 +1,31 @@
+//TiC Tac Toe - game between 2 players or player vs computer
+
+
+
+int main() {
+
+    //Prompt user for game we they wish to play
+
+    //Read information from console
+
+    //Start game, 2 player or player vs computer
+
+    //Print player/cpu turn
+
+    //Print game board
+
+    //Read information from console
+
+    //Check for winner
+
+    //Repeat last four steps until winner is found
+
+    //Print winner or tie
+
+    //Ask user if they want to play again
+
+    //If yes, restart game, if no terminate program
+
+
+    return 0;
+}
